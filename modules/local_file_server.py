ce76764eb8959182175dc4c3f6707a010753d51f
oneshell <oneshell@mbp.local>
2024-06-07 11:41:58 +0800

    add watcher

--- modules/local_file_server.py
+++ modules/local_file_server.py
@@ -3,16 +3,36 @@ import socketserver
 import argparse
 import logging
 import os
+import threading
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEvent, FileSystemEventHandler
 
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 from tftpy import TftpServer
-import threading
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(levelname)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
+
+def pack_rootfs(rootfs_path, output_path):
+    if os.path.exists(output_path):
+        os.remove(output_path)
+    shutil.make_archive(output_path, 'zip', rootfs_path)
+    logging.info("Root filesystem packed to %s", output_path)
+
+class RootFSHandler(FileSystemEventHandler):
+    def __init__(self, rootfs_path, output_path):
+        self.rootfs_path = rootfs_path
+        self.output_path = output_path
+
+    def on_any_event(self, event: FileSystemEvent) -> None:
+        if not event.is_directory:
+            logging.info("Root filesystem changed, packing it")
+            pack_rootfs(self.rootfs_path, self.output_path)
 
 class CustomHTTPHandler(http.server.SimpleHTTPRequestHandler):
     def log_message(self, format, *args):
@@ -32,6 +52,7 @@ class MultiServer:
     FTP_PORT = 8021
     TFTP_PORT = 8069
     ROOTFS = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../tools")
+    ARCHIVE = os.path.join(ROOTFS, "tools")
 
     def __init__(self, rootfs=None):
         self.http_server = None
@@ -39,6 +60,14 @@ class MultiServer:
         self.tftp_server = None
         self.rootfs = rootfs if rootfs else self.ROOTFS
         os.chdir(self.rootfs)
+        pack_rootfs(self.rootfs, self.ARCHIVE)
+        self.set_watcher()
+    
+    def set_watcher(self):
+        event_handler = RootFSHandler(self.rootfs, self.ARCHIVE)
+        observer = Observer()
+        observer.schedule(event_handler, self.rootfs, recursive=True)
+        observer.start()
 
     def start_http_server(self):
         handler = CustomHTTPHandler
